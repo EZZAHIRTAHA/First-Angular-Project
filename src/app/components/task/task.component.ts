1214a258d7b4ac75b8fd88eee3b0cd194a655579
Ezzahir Taha <taha1.ezzahir@gmail.com>
2023-08-28 01:16:13 +0100

    Feat: Added header and button components

---
+++ src/app/components/task/task.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TASKS } from '../../tasks';
+import {Task} from '../../Task'
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+export class TaskComponent {
+  
+}
